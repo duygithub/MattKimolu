95815a64b2c975d5e5865f8e38473280c109fc2b
bo-wang-1007 <bo_wang@berkeley.edu>
2025-11-22 20:21:59 -0800

    Updated Model
    1, Added 2 Models.
    2, Added descriptive summary by data entry type.
    3, Updated power analysis function.
    4, Updated descriptive summary for exploratory data.

---
+++ research/uc_berkeley/datasci203/source/descriptive_summary.R
@@ -0,0 +1,29 @@
+
+descriptive_summary <- function(prepped_data)  {
+  
+  summary_stats2 <-prepped_data %>%
+    group_by(stroke_count) %>%
+    summarize(n=length(unique(event_ordinal)),
+              Mean = mean(elapsed_time, na.rm = TRUE),
+              SD=sd(elapsed_time, na.rm = TRUE),
+              SE = sd(elapsed_time, na.rm = TRUE)/sqrt(length(elapsed_time)),
+              Min=quantile(elapsed_time,probs = 0,type=2,na.rm = TRUE) ,
+              Q1=quantile(elapsed_time,probs = 0.25,type=2,na.rm = TRUE),
+              Median= median(elapsed_time, na.rm = TRUE),
+              Q3=quantile(elapsed_time,probs = 0.75,type=2,na.rm = TRUE),
+              Max=quantile(elapsed_time,probs = 1,type=2,na.rm = TRUE)
+    )
+  
+  
+  ## Calculate Pooled SD
+  summary_stats2_sd <- summary_stats2 %>%
+    filter(!is.na(SD))
+  
+  pooled_sd <- with(summary_stats2_sd, sqrt(sum((n - 1) * SD^2) / sum(n - 1)))
+  
+  summary_stats2$Pooled_SD <- pooled_sd 
+  
+  return(summary_stats2)
+  
+}
+
