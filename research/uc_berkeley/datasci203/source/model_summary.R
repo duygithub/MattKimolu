9e54d2b35ebafc00d976fbc22e847b33e0a8e982
Duy Bao Vo <duyvo@berkeley.edu>
2025-11-30 23:22:04 +0000

    Updated model summary table

--- research/uc_berkeley/datasci203/source/model_summary.R
+++ research/uc_berkeley/datasci203/source/model_summary.R
@@ -9,3 +9,18 @@ calculate_model_coefficients_summary <- function(model) {
                r_squared = model_summary$r.squared,
                adjusted_r_squared = model_summary$adj.r.squared))
 }
+
+derive_detailed_analysis_by_stroke_count <- function(data_frame, data_entry_type_filter_value) {
+  data_frame %>%
+    filter(data_entry_type == data_entry_type_filter_value) %>%
+    group_by( stroke_count) %>%
+    summarize(n = length((event_ordinal)),
+              !!r"($\bar{x}$)" := mean(time_milliseconds, na.rm = TRUE),
+              !!r"($s$)" := sd(time_milliseconds, na.rm = TRUE),
+              !!r"($x_{\text{Fastest}}$)" := quantile(time_milliseconds,probs = 0,type=2,na.rm = TRUE) ,
+              !!r"($\tilde{x}$)" := median(time_milliseconds, na.rm = TRUE),
+              !!r"($s_{\bar{x}}$)" := sd(time_milliseconds, na.rm = TRUE)/sqrt(length(time_milliseconds)),
+              !!r"($x_{\text{Slowest}}$)" := quantile(time_milliseconds,probs = 1,type=2,na.rm = TRUE)
+    ) %>%
+    rename(!!r"($S_c$)" := stroke_count)
+}
